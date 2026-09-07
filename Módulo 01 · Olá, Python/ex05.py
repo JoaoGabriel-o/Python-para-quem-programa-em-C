8b0f400f4e94b9a42819cde89d47b5ede6dec487
João Gabriel <joaogabrieloliver4@gmail.com>
2026-09-06 23:56:49 -0300

    exercício 05

---
+++ Módulo 01 · Olá, Python/ex05.py
@@ -0,0 +1,10 @@
+# Exercício 5
+# Monte uma pequena ficha usando print(). Use um print() vazio para criar a linha em branco (em vez de colocar \n no fim do texto, como se faz em C).
+
+print("=== FICHA DO ALUNO ===")
+print("Nome:", "Ana")
+print("Curso:", "Medicina")  # complete aqui: linha do curso
+print("Período:", "2°")      # complete aqui: linha do período
+print()                      # linha em branco — sem \n
+print("Nota:", 8.5)
+print("Idade:", 19)          # complete aqui: imprima a idade usando print com dois valores
