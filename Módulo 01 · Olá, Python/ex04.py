3cd461d00ddaca247bd509aee1d5275e0c963836
João Gabriel <joaogabrieloliver4@gmail.com>
2026-09-06 23:48:53 -0300

    exercício 04 + desafio

---
+++ Módulo 01 · Olá, Python/ex04.py
@@ -0,0 +1,8 @@
+# Imprima a frase abaixo, com as aspas aparecendo na tela:
+# O professor disse "bom dia" para a turma
+
+print('O professor disse "bom dia" para a turma')
+
+# Desafio: agora imprima  It's Python!  (com o apóstrofo aparecendo)
+
+print("It's Python!")
