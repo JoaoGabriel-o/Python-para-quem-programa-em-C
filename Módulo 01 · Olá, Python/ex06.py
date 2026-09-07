41b99461642b253bf00953e5a7cf9b30140b625f
João Gabriel <joaogabrieloliver4@gmail.com>
2026-09-07 12:16:28 -0300

    exercício 06

---
+++ Módulo 01 · Olá, Python/ex06.py
@@ -0,0 +1,19 @@
+# Exercício 6 · Traduzindo C para Python
+
+# Abaixo está um programa em C. Escreva o equivalente em Python na célula de código.
+
+# include <stdio.h>
+# int main() {
+#    printf("Bem-vindo!\n");
+#    printf("Turma: %s\n", "Engenharia");
+#    printf("Ano: %d\n", 2026);
+#    return 0;
+# }
+
+# Exercício 6 — versão em Python do programa em C
+
+print("Bem vindo!") # complete aqui
+print("Turma:\nEngenharia") # complete aqui
+print("Ano:\n2026")# complete aqui
+# Quantas linhas você precisou? em 3 linhas eu fiz em python o mesmo codigo em C
+# Quantas sumiram? sumiram 4 linha, no codigo em C tinha 7
