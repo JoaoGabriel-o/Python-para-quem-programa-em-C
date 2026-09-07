77dbbb1e4ce2f23bd8f6dbc872903dce0934f7c1
João Gabriel <joaogabrieloliver4@gmail.com>
2026-09-06 23:37:26 -0300

    exercício 02

---
+++ Módulo 01 · Olá, Python/ex02.py
@@ -0,0 +1,6 @@
+# Exercício 2
+# Escreva três comandos print(): um com o seu nome, um com o seu curso e um com a sua cidade. Repare que cada print() já pula a linha sozinho.
+
+print("Meu nome é João Gabriel")  # complete aqui
+print("Sou estudante de Tecnologia em Inteligência Artificial no Piauí Instituto de Tecnologia (PIT)") # complete aqui
+print("Moro em Teresina Piauí")# complete aqui
